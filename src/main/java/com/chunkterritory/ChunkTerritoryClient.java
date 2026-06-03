573d87d695af8e01dfab5fe05fd74e5d9ba4fb54
rokosbasiliskburneracc <ssdfs7290@gmail.com>
2026-06-03 23:01:59 +0200

    Update ChunkTerritoryClient.java

--- src/main/java/com/chunkterritory/ChunkTerritoryClient.java
+++ src/main/java/com/chunkterritory/ChunkTerritoryClient.java
@@ -1,14 +1,12 @@
 package com.chunkterritory;
 
 import com.chunkterritory.gui.TerritoryScreen;
-import com.chunkterritory.render.ChunkBorderRenderer;
 import com.chunkterritory.render.TerritoryOverlayRenderer;
 import com.chunkterritory.territory.TerritoryManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -20,14 +18,13 @@ public class ChunkTerritoryClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("ChunkTerritory");
     public static final String MOD_ID = "chunkterritory";
 
-    // Keybind: Y to open GUI
     public static KeyBinding openGuiKey;
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("[ChunkTerritory] Initializing...");
 
-        // Register keybind
+        // KeyBinding constructor: (String translationKey, InputUtil.Type type, int code, String category)
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "key.chunkterritory.open_gui",
             InputUtil.Type.KEYSYM,
@@ -35,7 +32,6 @@ public class ChunkTerritoryClient implements ClientModInitializer {
             "category.chunkterritory"
         ));
 
-        // World join/leave events
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             String worldId = TerritoryManager.deriveWorldId(client);
             TerritoryManager.INSTANCE.onWorldJoin(worldId);
@@ -46,16 +42,12 @@ public class ChunkTerritoryClient implements ClientModInitializer {
             TerritoryManager.INSTANCE.onWorldLeave();
         });
 
-        // Client tick: handle keybind press + overlay sync
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openGuiKey.wasPressed()) {
                 client.setScreen(new TerritoryScreen(null));
             }
             TerritoryOverlayRenderer.tick();
         });
-
-        // World render: draw chunk borders in 3D
-        WorldRenderEvents.AFTER_ENTITIES.register(ChunkBorderRenderer::render);
 
         LOGGER.info("[ChunkTerritory] Ready! Press Y to manage territories.");
     }
